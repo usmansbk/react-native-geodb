9161bf06b7cc0ea4284cc95b20f54656dae8c8d3
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-04-26 15:54:48 +0100

    chore/update examples

--- examples/ExampleOne.js
+++ examples/ExampleOne.js
@@ -4,7 +4,7 @@ import GeoDBCitiesSearch, { Geocoder } from 'react-native-geodb';
 const GEODB_KEY = "rdcRtGlyhVmshYGy2m2p3jBCFSfOp1rrdtqjsn8tzV5y3RRSK3"
 
 export default () => {
-	
+	console.log('changed');	
 	Geocoder({lat: 10.52641, lng: 7.43879}, GEODB_KEY).then(response => console.log(response)).catch(error => console.log(error));
 	return (
 		<GeoDBCitiesSearch
