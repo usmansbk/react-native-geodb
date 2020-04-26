b6a1be055e3e37769f5a94c73d1d1e112b353f01
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-04-26 15:52:09 +0100

    chore

--- examples/ExampleOne.js
+++ examples/ExampleOne.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import GeoDBCitiesSearch, { Geocoder } from 'react-native-geodb';
 
+const GEODB_KEY = "rdcRtGlyhVmshYGy2m2p3jBCFSfOp1rrdtqjsn8tzV5y3RRSK3"
+
 export default () => {
-	Geocoder({lat: 10.52641, lng: 7.43879}).then(response => console.log(response)).catch(error => console.log(error));
+	
+	Geocoder({lat: 10.52641, lng: 7.43879}, GEODB_KEY).then(response => console.log(response)).catch(error => console.log(error));
 	return (
 		<GeoDBCitiesSearch
 			query={{
-				key: "rdcRtGlyhVmshYGy2m2p3jBCFSfOp1rrdtqjsn8tzV5y3RRSK3"
+				key: GEODB_KEY
 			}}
 			showActivityIndicator
 			progressViewOffset={300}
