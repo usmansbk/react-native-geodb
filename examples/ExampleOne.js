fcc46c528495e44583627df1bf2c31b3804a2a3e
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-04-26 15:30:46 +0100

    chore
    ;

--- examples/ExampleOne.js
+++ examples/ExampleOne.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import GeoDBCitiesSearch from 'react-native-geodb';
+import GeoDBCitiesSearch, { Geocoder } from 'react-native-geodb';
 
-export default () => (
-	<GeoDBCitiesSearch
-		query={{
-			key: "rdcRtGlyhVmshYGy2m2p3jBCFSfOp1rrdtqjsn8tzV5y3RRSK3"
-		}}
-		showActivityIndicator
-		progressViewOffset={300}
-	/>
-);
+export default () => {
+	Geocoder({}).then(response => console.log(response)).catch(error => console.log(error));
+	return (
+		<GeoDBCitiesSearch
+			query={{
+				key: "rdcRtGlyhVmshYGy2m2p3jBCFSfOp1rrdtqjsn8tzV5y3RRSK3"
+			}}
+			showActivityIndicator
+			progressViewOffset={300}
+		/>
+	);
+};
